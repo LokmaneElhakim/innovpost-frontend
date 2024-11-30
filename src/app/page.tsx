0967ca0d904463d2e626cc8f83c53048385405c6
Lokmane elhakim <baslilokmane04@gmail.com>
2024-11-30 23:20:11 +0100

    config: tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
+import HeroSection from "@/components/Hero";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
+  const { setTheme } = useTheme();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <button
         onClick={() =>
           setTheme((prev) => (prev === "light" ? "dark" : "light"))
         }
-      >
-        {theme}
-      </button>
+      ></button>
+      <HeroSection />
       <BackgroundBeams />
     </div>
   );
