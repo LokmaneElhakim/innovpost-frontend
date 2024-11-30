25dd85d4626c658cbe13de8a0091ac389d0ec3be
Lokmane elhakim <baslilokmane04@gmail.com>
2024-11-30 22:46:47 +0100

    feat : enabled dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
 import { BackgroundBeams } from "@/components/ui/background-beams";
-import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-      <BackgroundBeams/>
+      <button
+        onClick={() =>
+          setTheme((prev) => (prev === "light" ? "dark" : "light"))
+        }
+      >
+        {theme}
+      </button>
+      <BackgroundBeams />
     </div>
   );
 }
